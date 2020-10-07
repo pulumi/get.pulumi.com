f6fa532c71e453976a6d334a8f7508ffc4af0afd
Lee Briggs <lee@leebriggs.co.uk>
2020-10-07 14:14:37 -0700

    check perms of all files

--- spec/inspec/controls/pulumi.rb
+++ spec/inspec/controls/pulumi.rb
@@ -12,9 +12,24 @@ describe file("#{homedir}/.pulumi") do
   it { should be_directory }
 end
 
-describe file("#{homedir}/.pulumi/bin/pulumi") do
-  it { should exist }
-  its('type') { should eq :file }
-  its('mode') { should cmp '00755' }
-  its('size') { should be > 64 }
+installed_files = %w[
+  pulumi
+  pulumi-analyzer-policy
+  pulumi-analyzer-policy-python
+  pulumi-language-dotnet
+  pulumi-language-go
+  pulumi-language-nodejs
+  pulumi-language-python
+  pulumi-language-python-exec
+  pulumi-resource-pulumi-nodejs
+  pulumi-resource-pulumi-python
+]
+
+installed_files.each do |file|
+  describe file("#{homedir}/.pulumi/bin/#{file}") do
+    it { should exist }
+    its('type') { should eq :file }
+    its('mode') { should cmp '00755' }
+    its('size') { should be > 64 }
+  end
 end
